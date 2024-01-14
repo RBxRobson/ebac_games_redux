28b6ea156af44aa3171f112b48c7e63665e26751
Robson.B <robsonnbarbosa0@gmail.com>
2024-01-13 21:16:09 -0300

    tests - testes para o header

---
+++ src/components/Header/__tests__/header.test.tsx
@@ -0,0 +1,17 @@
+import { render, screen } from "@testing-library/react"
+import Header from ".."
+import { Provider } from "react-redux"
+import { store } from "../../../store"
+
+describe('testes para o componente header', () => {
+    test('deve renderizar corretamente', () => {
+        render(
+            //* Quando trabalhamos com o redux, para o teste funcionar
+            //* precisamos colocar o conteúdo de teste dentro do provider
+            <Provider store={store}>
+                <Header />
+            </Provider>
+        )
+        expect(screen.getByText('EBAC Games')).toBeInTheDocument()
+    })
+})
