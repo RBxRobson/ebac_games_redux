e2f38140ade80442b8007dd69f95cf60c1143240
Robson.B <robsonnbarbosa0@gmail.com>
2024-01-15 17:21:09 -0300

    add - configurações dos testes para o header

--- src/components/Header/__tests__/header.test.tsx
+++ src/components/Header/__tests__/header.test.tsx
@@ -1,18 +1,48 @@
-import { render, screen } from "@testing-library/react"
+import { screen } from "@testing-library/react"
 import Header from ".."
-import { Provider } from "react-redux"
-import { store } from "../../../store"
+import { renderizaComProvider } from "../../../utils/tests"
 
 describe('testes para o componente header', () => {
     test('deve renderizar corretamente', () => {
-        render(
-            
-            //* Quando trabalhamos com o redux, para o teste funcionar
-            //* precisamos colocar o conteúdo de teste dentro do provider
-            <Provider store={store}>
-                <Header />
-            </Provider>
-        )
+        // render(
+        //     //* Quando trabalhamos com o redux, para o teste funcionar
+        //     //* precisamos colocar o conteúdo de teste dentro do provider
+        //     <Provider store={store}>
+        //         <Header />
+        //     </Provider>
+        // )
+        renderizaComProvider(<Header />)
         expect(screen.getByText('EBAC Games')).toBeInTheDocument()
+    })
+
+    test('deve renderizar com dois itens no carrinho', ()=> {
+        renderizaComProvider(<Header />, {
+            preloadedState: {
+                carrinho: {
+                    itens: [
+                        {
+                            id: 1,
+                            categoria: 'RPG',
+                            imagem: '',
+                            plataformas: ['Windowns'],
+                            preco: 150.9,
+                            precoAntigo: 199.9,
+                            titulo: 'Elden Ring'
+                        },
+                        {
+                            id: 2,
+                            categoria: 'RPG',
+                            imagem: '',
+                            plataformas: ['Windowns', 'PS5', 'Xbox S'],
+                            preco: 199.9,
+                            precoAntigo: 299.9,
+                            titulo: 'Hogwards Legacy'
+                        }
+                    ]
+                }
+            }
+        })
+
+        expect(screen.getByTestId('qtd-carrinho').innerHTML).toContain('2 itens')
     })
 })
